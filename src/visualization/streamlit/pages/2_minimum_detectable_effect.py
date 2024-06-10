8c91f693c370b7845ec9cc719b35dd5b993653ed
stanislav.babenyshev@external.telekom.de <stanislav.babenyshev@external.telekom.de>
2024-06-10 17:12:09 +0200

    added session state saving for uploaded file

--- src/visualization/streamlit/pages/2_minimum_detectable_effect.py
+++ src/visualization/streamlit/pages/2_minimum_detectable_effect.py
@@ -14,17 +14,20 @@ if "cfg" not in st.session_state:
     dir_path = os.path.dirname(os.path.realpath(__file__)).replace("pages", "")
     st.session_state["cfg"] = read_yaml(f"{dir_path}default.yml")
 
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    with st.expander("see data", expanded=False):
-        st.write(df)
 
-    columns = list(df.columns)
+file = st.file_uploader("Choose a CSV file", type="csv")
+if file is not None:
+    st.session_state["data_mde"] = pd.read_csv(file)
+
+if "data_mde" in st.session_state.keys():
+    with st.expander("see data", expanded=False):
+        st.write(st.session_state["data_mde"])
+
+    columns = list(st.session_state["data_mde"].columns)
     selected_cols = st.multiselect("variables", options=columns)
 
     if selected_cols:
-        analyzer = SampleSizeGrid(variables=selected_cols, data=df)
+        analyzer = SampleSizeGrid(variables=selected_cols, data=st.session_state["data_mde"])
         distributions = analyzer.plot_distributions(return_fig=True)
         st.plotly_chart(distributions, use_container_width=True, theme="streamlit")
         obs_needed = analyzer.plot_observation_requirements(return_fig=True)
